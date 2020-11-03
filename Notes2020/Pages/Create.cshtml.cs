b07cd122809c9c2402f0e8139607faf0c2ebff07
Michal Stehlík <michal.stehlik@pslib.cz>
2020-11-03 15:09:10 +0100

    User Notes create and List

---
+++ Notes2020/Pages/Create.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Notes2020.Data;
+using Notes2020.Models;
+using Notes2020.ViewModels;
+
+namespace Notes2020.Pages
+{
+    [Authorize]
+    public class CreateModel : PageModel
+    {
+        private readonly Notes2020.Data.ApplicationDbContext _context;
+
+        public CreateModel(Notes2020.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public NoteIM Input { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var UserId = User.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).FirstOrDefault().Value;
+
+            Note newNote = new Note
+            {
+                Title = Input.Title,
+                Text = Input.Text,
+                UserId = UserId
+            };
+
+            _context.Notes.Add(newNote);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./List");
+        }
+    }
+}
---
+++ Notes2020/Pages/List.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Notes2020.Data;
+using Notes2020.Models;
+
+namespace Notes2020.Pages
+{
+    [Authorize]
+    public class ListModel : PageModel
+    {
+        private readonly Notes2020.Data.ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _um;
+
+        public ListModel(Notes2020.Data.ApplicationDbContext context, UserManager<IdentityUser> um)
+        {
+            _context = context;
+            _um = um;
+        }
+
+        public IList<Note> Note { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            var userId1 = User.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).FirstOrDefault().Value; // id
+            //var user = await _um.GetUserAsync(User); // kompletní user
+            //var userId = _um.GetUserId(User); // id
+            Note = await _context.Notes.Where(n => n.UserId == userId1).ToListAsync();
+        }
+    }
+}
